0f182b85a67f5a19b5bcc6c12e0ac6dfcf3b72ea
YuTera58 <YuTerakoya4Study@gmail.com>
2025-04-09 22:08:58 +0900

    Create Repository Java_Basic_kadai

---
+++ kadai_015/CarExec_Chapter15.java
@@ -0,0 +1,16 @@
+package kadai_015;
+
+public class CarExec_Chapter15
+{
+	public static void main(String[] args)
+	{
+		//Car_Chapter15 インスタンスの生成
+		Car_Chapter15 carChapture15 = new Car_Chapter15();
+		
+		//Car_Chapter15 クラスの gearChange() メソッドの実行
+		carChapture15.gearChange(3);
+		
+		//Car_Chapter15 クラスの run() メソッド実行
+		carChapture15.run();
+	}
+}
