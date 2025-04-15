051145f105de038fb4444ea961d17d93311a64f6
YuTera58 <YuTerakoya4Study@gmail.com>
2025-04-15 22:54:43 +0900

    fix kadai_015

--- kadai_015/Car_Chapter15.java
+++ kadai_015/Car_Chapter15.java
@@ -2,35 +2,30 @@ package kadai_015;
 
 public class Car_Chapter15
 {
-	//1速から5速のギアを表す
-	private	int	gear = 1;
-	
-	//ギアチェンジ後の速度を表す
-	private	int	speed = 10;
-	
-	
-	//ギアの値により速度を変える
+	// 1速から5速のギアを表す
+	private int gear = 1;
+
+	// ギアチェンジ後の速度を表す
+	private int speed = 10;
+
+	// ギアの値により速度を変える
 	public void gearChange(int afterGear)
-<<<<<<< HEAD
 	{
-=======
-        {
->>>>>>> 13afbe86aed2e833e504462e665989a859bfd02f
 		System.out.println("ギア" + gear + "から" + afterGear + "に切り替えました");
 		gear = afterGear;
-		
-		speed = switch(gear)
+
+		speed = switch (gear)
 		{
-			case 1  -> 10;
-			case 2  -> 20;
-			case 3  -> 30;
-			case 4  -> 40;
-			case 5  -> 50;
+			case 1 -> 10;
+			case 2 -> 20;
+			case 3 -> 30;
+			case 4 -> 40;
+			case 5 -> 50;
 			default -> 10;
 		};
-        }
-	
-	//ギアチェンジ後の速度を表示する
+	}
+
+	// ギアチェンジ後の速度を表示する
 	public void run()
 	{
 		System.out.println("速度は時速" + speed + "kmです");
